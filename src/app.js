c71e313f119a5a92acfef2a0b075c3dcb0ddfacd
harisudhan <m.harisudhan95@gmail.com>
2019-05-01 12:08:14 +0530

    handle barss added for the routes and minimal styling UI

--- src/app.js
+++ src/app.js
@@ -1,29 +1,32 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
 
-const app = express()
+const app = express();
 
 /** paths for express and hbs config */
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
 const dirPath = path.join(__dirname, '../public');
+const partialPath = path.join(__dirname, '../templates/partials');
 
 /** dynamic templating - setup hbs */
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialPath);
 
 /** static pages served through express */
 app.use(express.static(dirPath))
 
 app.get('', (req, res) => {
-    res.render('index', { title: 'Weather app'})
+    res.render('index', { title: 'Weather app', text: 'Home Page'})
 })
 
 app.get('/about', (req, res) => {
-    res.render('about', { title: 'Weather about' })
+    res.render('about', { title: 'About me', text: 'About page' })
 })
 
 app.get('/help', (req, res) => {
-    res.render('help', { help: 'help page' })
+    res.render('help', { title: 'Help', text: 'help page' })
 })
 
 app.get('/weather', (req, res) => {
@@ -32,6 +35,14 @@ app.get('/weather', (req, res) => {
         location: 'chennai'
     });
 })
+
+app.get('/help/*', (req, res) => {
+    res.render('404', { title: 'Weather Application', errorText: 'Help page not found' });
+})
+
+app.get('*', (req, res) => {
+    res.render('404', { title: 'Weather Application', errorText: 'My 404 Page' });
+});
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000')
